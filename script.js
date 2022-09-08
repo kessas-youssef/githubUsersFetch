04345f94e8b2aaa667ba8efc5111e14a2f2c3aac
Youssef <cefyou2002@gmail.com>
2022-09-08 23:03:24 +0100

    optimised Script and Styled responsively

--- script.js
+++ script.js
@@ -1,41 +1,63 @@
 // Select buttons
-
 const [allUsersBtn, oneUserInput] = [document.querySelector('.nav__btn--allUsers'), document.querySelector('.nav__input')]
 
-allUsersBtn.addEventListener('click', getAllUsers)
-oneUserInput.addEventListener('input', getUser)
 
-function getAllUsers() {
+// Get all users
+allUsersBtn.addEventListener('click', () => getData((users) => {
+    let output = ''
+    output += '<h1>All USERS</h1>'
+    users.forEach(user => {
+        output += `<div class="user">
+        <img class="user__img" src="${user.avatar_url}" alt="avatar of login: ${user.login}, id: ${user.id}">
+        <ul class="user__list">
+        <li class="user__infos">Login : ${user.login}</li>
+        <li class="user__infos">ID : ${user.id}</li>
+        </ul>
+        </div>`
+    });
+    document.querySelector("main").innerHTML += output
+}
+    ,
+    'https://api.github.com/users'))
+
+
+// Get user by "Login"
+oneUserInput.addEventListener('input', () => getData((user) => {
+
+    let output = ''
+    output += '<h1 class="successFound">Found successfully :</h1>'
+
+    output += `<div class="user">
+            <img class="user__img" src="${user.avatar_url}" alt="avatar of login: ${user.login}, id: ${user.id}">
+            <ul class="user__list">
+            <li class="user__infos">Login : ${user.login}</li>
+            <li class="user__infos">ID : ${user.id}</li>
+            </ul>
+            </div>`
+    document.querySelector('main').innerHTML += output
+}
+    ,
+    `https://api.github.com/users/${document.querySelector('.nav__input').value}`))
+
+
+function getData(whatToDo, source) {
+    emptyMain()
     const xhr = new XMLHttpRequest()
-    xhr.open('GET', 'https://api.github.com/users', true)
+    xhr.open('GET', source, true)
 
     xhr.onload = function () {
         if (this.status == 200) {
-            const users = JSON.parse(this.responseText)
-            let output = ''
-            output += '<h1>All USERS</h1>'
-            users.forEach(user => {
-                output += `<div class="user">
-                <img class="user__img" src="${user.avatar_url}" alt="avatar of login: ${user.login}, id: ${user.id}">
-                <ul class="user__list">
-                <li class="user__infos">Login : ${user.login}</li>
-                <li class="user__infos">ID : ${user.id}</li>
-                </ul>
-                </div>`
-            });
-            document.querySelector("main").innerHTML += output
-
+            const data = JSON.parse(this.responseText)
+            whatToDo(data)
         }
-
+        else if (this.status == 404) {
+            document.querySelector('main').innerHTML += '<h1 class="errorNotFound"> Not Found!</h1'
+        }
     }
     xhr.send()
-
 }
 
-function getUser() {
-
-    let inputValue = document.querySelector('.nav__input')
-
+const emptyMain = () => {
     // Empty the display Area
     document.querySelector('main').remove()
     let error = document.querySelector('.errorNotFound')
@@ -45,34 +67,6 @@ function getUser() {
     // Recreate the Main tag
     let main = document.createElement('main')
     document.body.appendChild(main)
-
-    // AJAX the Github API
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', `https://api.github.com/users/${inputValue.value}`, true)
-
-    xhr.onload = function () {
-        if (this.status == 200) {
-
-            const user = JSON.parse(this.responseText)
-
-            let output = ''
-            output += 'Found successfully :'
-
-            output += `<div class="user">
-            <img class="user__img" src="${user.avatar_url}" alt="avatar of login: ${user.login}, id: ${user.id}">
-            <ul class="user__list">
-            <li class="user__infos">Login : ${user.login}</li>
-            <li class="user__infos">ID : ${user.id}</li>
-            </ul>
-            </div>`
-            document.querySelector('main').innerHTML += output
-        } else if (this.status == 404) {
-            document.querySelector("main").innerHTML += '<h1 class="errorNotFound"> Not Found!</h1'
-        }
-    }
-    xhr.send()
 }
-
-
 
 
